4f6b4f9d723faa3c86d182b5702872c53a48eb7c
cristina-lopez <105756863+cristina-lopez@users.noreply.github.com>
2022-08-02 09:47:08 -0700

    deleting a course works

--- client/src/components/CourseDetails.js
+++ client/src/components/CourseDetails.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { Link, useParams, useHistory } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown'
 import Context from '../Context';
+import { Buffer } from "buffer";
 
 export default function CourseDetails() {
 
@@ -37,9 +38,12 @@ export default function CourseDetails() {
 
             fetch(`http://localhost:5000/api/courses/${id}`, {
                 method: "DELETE",
-                headers: { "Content-Type": "application/json" },
+                headers: { "Content-Type": "application/json",
+                    'Authorization': 'Basic ' + Buffer.from(
+                        `${context.authenticatedUser.emailAddress}:${context.authenticatedUser.password}`
+                      ).toString("base64") },
                 body: null,
-                credentials: 'same-origin',
+                
             })
             .then( response => {
                 if (response.status === 204) {
